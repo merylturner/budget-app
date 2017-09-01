bf3219009278131dc2f4b46951870bd254331bda
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-09-01 09:35:31 -0700

    implement modal on dashboard

--- app/src/Dashboard.js
+++ app/src/Dashboard.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import CategoryForm from './category/CategoryForm';
+import ReactModal from 'react-modal';
 
 export default class Dashboard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        };
+
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    handleOpenModal() {
+        this.setState({showModal: true});
+    }
+
+    handleCloseModal() {
+        this.setState({showModal: false});
+    }
+
     render(){
         return(
             <div>
-                <CategoryForm />
+                <button onClick={this.handleOpenModal}>+</button>
+                <ReactModal
+                    isOpen={this.state.showModal}
+                    contentLabel="Dashboard">
+                    <CategoryForm />
+                    <button onClick={this.handleCloseModal}>X</button>
+                </ReactModal>
             </div>
         )
     }
