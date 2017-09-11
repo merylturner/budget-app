41007c0d373eea0c24eab8eb7791c772bc5bbe17
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-09-11 12:20:24 -0700

    create auth files/folder, save button nonfunctional

---
+++ src/home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const GetStarted = () => (
+    <p>
+        <Link to="/auth/signin">Signin</Link>
+        {' or '}
+        <Link to="/auth/signup">Signup</Link>
+        {' to get started.'}
+    </p>
+);
+
+function Home({user}) {
+    return (
+        <div>
+            <h1>Welcome to Budge It!</h1>
+            <GetStarted></GetStarted>
+        </div>
+    );
+}
+
+export default connect(
+    state => ({user: state.auth.user})
+)(Home);
